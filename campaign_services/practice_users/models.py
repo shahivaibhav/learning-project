bb165e993095c61003eb9089cc076a7d0b837f8b
shahivaibhav <shahivaibhav1605@gmail.com>
2025-01-20 21:19:11 +0530

    Practice User Model Complete

--- campaign_services/practice_users/models.py
+++ campaign_services/practice_users/models.py
@@ -1,19 +1,24 @@
-from sqlalchemy import MetaData, Column, Integer, String, DateTime, create_engine, ForeignKey
-from sqlalchemy .orm import sessionmaker
+from sqlalchemy import MetaData, Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from django.contrib.auth.models import User
 
+# Database connection string
 DATABASE_URL = "postgresql://practice_user:Vaibhav1@localhost/campaigdb"
-metadata = MetaData()
-Base = declarative_base(metadata=metadata)
-engine = create_engine(DATABASE_URL)
-session = sessionmaker(bind=engine)
 
+# SQLAlchemy setup
+metadata = MetaData()
+Base = declarative_base()
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# SQLAlchemy model
 class PracticeUser(Base):
     __tablename__ = 'users'
 
-    id  = Column(Integer,primary_key=True)
-    user_id = Column(Integer, ForeignKey(User), ondelete='CASCADE')
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('auth_user.id', ondelete='CASCADE'))  # Reference Django's User table
     roles = Column(String)
 
+# Create tables
 Base.metadata.create_all(engine)
